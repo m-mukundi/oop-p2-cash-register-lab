fc8b33d00d2fcb8acc7d1796724fd877e50d1805
Mercy Mukundi <mercy.n.mukundi@gmail.com>
2026-07-16 11:34:56 +0300

    Attributes updated to public

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -3,45 +3,45 @@
 
 class CashRegister:
     def __init__(self, discount=0):
-        self._items = []
-        self._discount = discount
-        self._total = 0
-        self._previous_transactions = []
+        self.items = []
+        self.discount = discount
+        self.total = 0
+        self.previous_transactions = []
 
     @property
     def discount(self):
-        return self._discount
+        return self.discount
 
     @discount.setter
     def discount(self, value):
         if type(value) is int and 0 <= value <= 100:
-            self._discount = value
+            self.discount = value
         else:
             print("Not valid discount")
 
     def add_item(self, item, price, quantity):
-        self._items.append(item)
-        self._total += price * quantity
+        self.items.append(item)
+        self.total += price * quantity
         new_item = {"item": item, "price": price, "quantity": quantity}
-        self._previous_transactions.append(new_item)
+        self.previous_transactions.append(new_item)
 
     def apply_discount(self):
-        if self._discount > 0:
-            self._total = self._total * (100 - self._discount) / 100
+        if self.discount > 0:
+            self.total = self._total * (100 - self._discount) / 100
         else:
             print("There is no discount to apply.")
 
     def void_last_transaction(self):
-        if len(self._previous_transactions) > 0:
-            self._total = 0
-            for transaction in self._previous_transactions:
-                self._total += transaction["price"] * transaction["quantity"]
+        if len(self.previous_transactions) > 0:
+            self.total = 0
+            for transaction in self.previous_transactions:
+                self.total += transaction["price"] * transaction["quantity"]
 
-            last_transaction = self._previous_transactions[-1]
+            last_transaction = self.previous_transactions[-1]
 
-            self._items.remove(last_transaction["item"])
-            self._total -= last_transaction["price"] * last_transaction["quantity"]
-            self._previous_transactions.remove(last_transaction)
+            self.items.remove(last_transaction["item"])
+            self.total -= last_transaction["price"] * last_transaction["quantity"]
+            self.previous_transactions.remove(last_transaction)
         else:
             print("You have no previous transactions")
 
