43711514d6b430b49033183c6499981f9eba901b
Mercy Mukundi <mercy.n.mukundi@gmail.com>
2026-07-16 14:08:38 +0300

    add_items method updated

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -19,7 +19,7 @@ class CashRegister:
         else:
             print("Not valid discount")
 
-    def add_item(self, item, price, quantity):
+    def add_item(self, item, price, quantity=1):
         self.items.append(item)
         self.total += price * quantity
         new_item = {"item": item, "price": price, "quantity": quantity}
